371b46aff8b2c5b59c7419068973a98f63d7f729
Alan Lo <alan-lo@users.noreply.github.com>
2017-03-18 15:43:40 -0700

    Solutions and Drills Controller

--- app/controllers/drills_controller.rb
+++ app/controllers/drills_controller.rb
@@ -1,2 +1,46 @@
 class DrillsController < ApplicationController
+  before_action :find_drill , only: [:destroy, :update, :edit]
+
+  def create
+    @drill = Drill.new(drill_params)
+    @drill_group = DrillGroup.find params[:drill_group_id]
+    @drill.drill_group = @drill_group
+    if @drill.save
+      redirect_to @drill_group
+    else
+      redirect_to @drill_group, notice: 'Fail to create Drill'
+    end
+  end
+
+  def update
+    @drill_group = DrillGroup.find params[:drill_group_id]
+    @drill.drill_group = @drill_group
+    if @drill.update params[:id]
+      redirect_to @drill_group, notice: 'Update successful'
+    else
+      redirect_to @drill_group, notice: 'Update Failed'
+    end
+  end
+
+  def edit
+    @drill_group = DrillGroup.find params[:drill_group_id]
+    @drill.drill_group = @drill_group
+  end
+
+  def destroy
+    if @drill.destroy
+      redirect_to @drill_group, notice: 'Drill is destroyed'
+    else
+      redirect_to @drill_group, notice: 'Cannot destroy View'
+    end
+  end
+
+  private
+  def find_drill
+    @drill = Drill.find params[:id]
+  end
+
+  def drill_params
+    drill_params =  params.require(:drill).permit(:description, :drill_group_id)
+  end
 end
--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,2 +1,47 @@
 class SolutionsController < ApplicationController
+
+  before_action :find_solution , only: [:create, :destroy, :edit]
+
+  def create
+    @solution = Solution.new(solution_params)
+    @drill = Drill.find params[:drill_id]
+    @solution.drill = @solution
+    if @solution.save
+      redirect_to @drill_group
+    else
+      redirect_to @drill_group, alert: 'Fail to create solution'
+    end
+  end
+
+  def destroy
+    if @solution.destroy
+      redirect_to @drill_group, notice: 'Solution is destroyed'
+    else
+      redirect_to @drill_group, alert: 'Cannot destroy solution'
+    end
+  end
+
+  def edit
+  end
+
+  # implement later
+  # def update
+  #   @drill_group = DrillGroup.find params[:drill_group_id]
+  #   @drill.drill_group  = @drill_group
+  #   @solution.drill = @drill
+  #   if @solution.update params[:form_field]
+  #     redirect_to @drill_group, notice: 'Update successful'
+  #   else
+  #     redirect_to @drill_group, alert: 'Update Failed'
+  #   end
+  # end
+
+  private
+  def find_solution
+    @solution = Solution.find params[:id]
+  end
+
+  def solution_params
+      solution_params = params.require(:solution).permit(:body)
+  end
 end
