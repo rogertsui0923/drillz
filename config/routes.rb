ff3c06b8907f286ab33b9ccdb1e90869f7e80eaf
Roger Tsui <rogertsui0923@gmail.com>
2017-03-18 00:22:37 -0700

    add thoughts.txt regarding necessary changes to db

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
+  # leaderboard could use user#index
+  # the profile link on the wireframe could direct to user#show
+  resources :sessions, only: [:new, :create, :destroy]
+
+  resources :drill_groups
 end
