ba72ab1e3aa7ca906d1413c10ed121af2c0d3172
Nari Roh <nari.rohh@gmail.com>
2017-03-19 16:16:47 -0700

    Redirect user when group session is done

--- app/controllers/group_sessions_controller.rb
+++ app/controllers/group_sessions_controller.rb
@@ -1,10 +1,15 @@
 class GroupSessionsController < ApplicationController
-
   def create
-    groupSession = GroupSession.new(user: current_user , drill_group: params[:drill_group_id])
-    if groupSession.save
-      redirect_to drill_path(groupSession.drill_group.drills.first)
-    else
-    end
+    group_session = GroupSession.new(user: current_user,
+                                     drill_group_id: params[:drill_group_id])
+
+    group_session.save
+    redirect_to drill_path(group_session.drill_group.drills.first)
   end
+
+  def show
+    @group_session = GroupSession.find params[:id]
+    @drill_group = @group_session.drill_group
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'users#index' # <---- change this
-  
+
   resources :passwords_resets, only: [:new, :create, :edit, :update]
 
 
@@ -12,15 +12,14 @@ Rails.application.routes.draw do
     get '/profile' => 'users#profile'
   end
 
-
   # leaderboard could use user#index
   # the profile link on the wireframe could direct to user#show
   resources :sessions, only: [:new, :create, :destroy]
 
   resources :drill_groups, shallow: true do
-    resources :drills, only: [:create, :update, :destroy, :edit, :show] do
-      resources :solutions#, only: [
-      resources :attempts
+    resources :group_sessions, only: [:create, :show]
+    resources :drills, except: [:new, :index] do
+      resources :attempts, only: :create
     end
   end
 
