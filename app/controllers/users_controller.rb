f94d6a3b84cd6625fe3efd63e73f051799f232f4
Fernando Huber <fernando.huber@gmail.com>
2017-03-19 19:39:50 -0700

    update issue fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
     user_params = params.require(:user).permit(:first_name, :last_name, :email)
     @user = current_user
     if @user.update(user_params)
-      redirect_to root_path(@user)
+      redirect_to user_path(@user), notice: 'Account updated!'
     else
       render :edit
     end
