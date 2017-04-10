143d3ae329ce1bd8dae1795b605a83e06654695f
Roger Tsui <rogertsui0923@gmail.com>
2017-04-10 15:21:14 -0700

    modify seed file for deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-leader = User.new(first_name: 'Basia', last_name: 'C', email: 'basia@c.com', password: 'secret', is_admin: true, is_approved: true)
-leader.save
-User.create(first_name: 'Kira', last_name: 'G', email: 'kira@g.com', password: 'secret', is_admin: true, is_approved: true)
-User.create(first_name: 'Nari', last_name: 'R', email: 'nari@r.com', password: 'secret', is_admin: true, is_approved: true)
-User.create(first_name: 'Mitchell', last_name: 'W', email: 'mitchell@w.com', password: 'secret', is_approved: true)
-User.create(first_name: 'Fernando', last_name: 'H', email: 'fernando@h.com', password: 'secret', is_approved: true)
-User.create(first_name: 'Alan', last_name: 'L', email: 'alan@l.com', password: 'secret')
-User.create(first_name: 'Roger', last_name: 'T', email: 'roger@t.com', password: 'secret')
+# leader = User.new(first_name: 'Basia', last_name: 'C', email: 'basia@c.com', password: 'secret', is_admin: true, is_approved: true)
+# leader.save
+# User.create(first_name: 'Kira', last_name: 'G', email: 'kira@g.com', password: 'secret', is_admin: true, is_approved: true)
+# User.create(first_name: 'Nari', last_name: 'R', email: 'nari@r.com', password: 'secret', is_admin: true, is_approved: true)
+# User.create(first_name: 'Mitchell', last_name: 'W', email: 'mitchell@w.com', password: 'secret', is_approved: true)
+# User.create(first_name: 'Fernando', last_name: 'H', email: 'fernando@h.com', password: 'secret', is_approved: true)
+# User.create(first_name: 'Alan', last_name: 'L', email: 'alan@l.com', password: 'secret')
+User.create(first_name: 'Roger', last_name: 'Tsui', email: 'rogertsui0923@gmail.com', password: 'secret', is_admin: true, is_approved: true)
 
-ruby_hash = DrillGroup.new(name: 'Ruby Hashes', description: 'Drills involving Ruby hashes', level: 'beginner', points: 100)
-js_string = DrillGroup.create(name: 'Javascript Strings', description: 'Drills involving Javascript strings', level: 'difficult', points: 400)
-ruby_hash.save
-js_string.save
-
-d1 = Drill.new(description: 'Given that h = {a: 1}, access the value 1 corresponding to key "a".', drill_group: ruby_hash)
-d2 = Drill.new(description: 'Given that h = {a: 1, b: 2}, iterate through the hash and print out the keys.', drill_group: ruby_hash)
-d3 = Drill.new(description: 'Given s = "hello world", return ["hello", "world"].', drill_group: js_string)
-d1.save
-d2.save
-d3.save
-
-Solution.create(body: 'h[:a]', drill: d1)
-Solution.create(body: 'h.each { |k, v| puts k }', drill: d2)
-Solution.create(body: 'h.each do |k, v| puts k end', drill: d2)
-Solution.create(body: 's.split(" ")', drill: d3)
+# ruby_hash = DrillGroup.new(name: 'Ruby Hashes', description: 'Drills involving Ruby hashes', level: 'beginner', points: 100)
+# js_string = DrillGroup.create(name: 'Javascript Strings', description: 'Drills involving Javascript strings', level: 'difficult', points: 400)
+# ruby_hash.save
+# js_string.save
+#
+# d1 = Drill.new(description: 'Given that h = {a: 1}, access the value 1 corresponding to key "a".', drill_group: ruby_hash)
+# d2 = Drill.new(description: 'Given that h = {a: 1, b: 2}, iterate through the hash and print out the keys.', drill_group: ruby_hash)
+# d3 = Drill.new(description: 'Given s = "hello world", return ["hello", "world"].', drill_group: js_string)
+# d1.save
+# d2.save
+# d3.save
+#
+# Solution.create(body: 'h[:a]', drill: d1)
+# Solution.create(body: 'h.each { |k, v| puts k }', drill: d2)
+# Solution.create(body: 'h.each do |k, v| puts k end', drill: d2)
+# Solution.create(body: 's.split(" ")', drill: d3)
